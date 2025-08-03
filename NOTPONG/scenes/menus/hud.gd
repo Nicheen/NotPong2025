c91e3646b0d73bb70f59973deadb29d6b4689ad1
Nicheen <46378903+Nicheen@users.noreply.github.com>
2025-08-03 13:24:16 +0200

    Added Timer

--- NOTPONG/scenes/menus/hud.gd
+++ NOTPONG/scenes/menus/hud.gd
@@ -3,6 +3,7 @@ class_name HUD extends CanvasLayer
 @onready var level: Label = %Level
 @onready var score: Label = %Score
 @onready var high_score: Label = %HighScore
+@onready var timer: Label = %Timer
 
 func _ready() -> void:
 	high_score.text = "High Score: " + str(Global.save_data.high_score)
@@ -12,3 +13,9 @@ func update_score(n:int):
 
 func update_level(current_level:int):
 	level.text = "Level: " + str(current_level)
+	
+func update_timer(current_time: float):
+	var minutes = int(current_time) / 60
+	var seconds = int(current_time) % 60
+	var milliseconds = int((current_time - int(current_time)) * 1000)
+	timer.text = "%02d:%02d.%03d" % [minutes, seconds, milliseconds]
